2c5ae7531a4bdff654dc726109ac7496fea7d61b
oikosohn <59533593+oikosohn@users.noreply.github.com>
2021-12-06 06:15:14 +0900

    Create jaccard_ce_loss_smp.py

---
+++ jaccard_ce_loss_smp.py
@@ -0,0 +1,18 @@
+import segmentation_models_pytorch as smp
+import torch.nn as nn
+
+class JaccardCELoss(smp.utils.base.Loss):
+    def __init__(self, alpha=1.0, beta=0.5, **kwargs):
+        super().__init__(**kwargs)
+        self.alpha = alpha
+        self.beta = beta
+
+		# weight 추가할 경우 class에 할당
+        self.jaccardloss=smp.losses.JaccardLoss(mode='multiclass')
+        self.jaccardloss.__name__ = 'jaccard_loss'
+
+        self.celoss = nn.CrossEntropyLoss()
+        self.celoss.__name__ = 'ce_loss'
+
+    def forward(self, y_pred, y_true):
+        return self.alpha * self.celoss.forward(y_pred, y_true) - self.beta * self.jaccardloss.forward(y_pred, y_true)
